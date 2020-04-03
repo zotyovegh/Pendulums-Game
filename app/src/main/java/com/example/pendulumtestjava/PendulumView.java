78244ecb76a7df2c6cf05f6a8e233cfd31e1d060
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-04-03 18:29:33 +0200

    Speed issue

--- app/src/main/java/com/example/pendulumtestjava/PendulumView.java
+++ app/src/main/java/com/example/pendulumtestjava/PendulumView.java
@@ -1,18 +1,17 @@
 package com.example.pendulumtestjava;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.view.View;
 import androidx.annotation.Nullable;
 
 public class PendulumView extends View {
+
+
     Paint paintCircle, paintThread;
     Path pathThread1, pathThread2;
 
@@ -29,33 +28,29 @@ public class PendulumView extends View {
     float y2;
     float a1_v = 0;
     float a2_v = 0;
-//    float a1_a = (float)0.01;
-//    float a2_a = -(float)0.01;
+    float a1_a = (float)0.01;
+    float a2_a = -(float)0.01;
     float g = 1;
 
 
      public PendulumView(Context context) {
         super(context);
-        init();
     }
 
     public PendulumView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init();
     }
 
     public PendulumView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
         int xCenter = getWidth() / 2;
-
-
-
+        //Some initialization and constants
         paintCircle = new Paint();
         paintThread = new Paint();
         paintCircle.setStyle(Paint.Style.FILL);
@@ -70,7 +65,14 @@ public class PendulumView extends View {
         pathThread1.moveTo(0, 0);
         pathThread2.moveTo(x1, y1);
 
-//
+
+        //coordinate settings
+        x1 = r1 * (float)Math.sin(Math.toRadians(a1));
+        y1 = r1 * (float)Math.cos(Math.toRadians(a1));
+        x2 = x1 + r2 * (float)Math.sin(Math.toRadians(a2));
+        y2 = y1 + r2 * (float)Math.cos(Math.toRadians(a2));
+
+        //actual calculations ***turned off, as long as figure out the speed issue
 //        float num1 = -g * (2 * m1 + m2) * (float)Math.sin(Math.toRadians(a1));
 //        float num2 = -m2 * g * (float)Math.sin(Math.toRadians(a1 - 2 * a2));
 //        float num3 = -2 * (float)Math.sin(Math.toRadians(a1 - a2)) * m2;
@@ -84,48 +86,25 @@ public class PendulumView extends View {
 //        num4 = a2_v * a2_v * r2 * m2 * (float)Math.cos(Math.toRadians(a1 - a2));
 //        den = r2 * (2 * m1 + m2 - m2 * (float)Math.cos(Math.toRadians(2 * a1 - 2 * a2)));
 //        float a2_a = (num1 * (num2 + num3 + num4)) / den;
-
-
-
-
-
-
-        x1 = r1 * (float)Math.sin(Math.toRadians(a1));
-        y1 = r1 * (float)Math.cos(Math.toRadians(a1));
-        x2 = x1 + r2 * (float)Math.sin(Math.toRadians(a2));
-        y2 = y1 + r2 * (float)Math.cos(Math.toRadians(a2));
-
-
+//
+//
 //        a1_v += a1_a;
 //        a2_v += a2_a;
 //        a1 += a1_v;
 //        a2 += a2_v;
-
-        a1 += 1;
+        //For testing the speed **the app should be very fast, when we are incrementing the angles by 0.1
+        a1 += 1.0; //even with 1 it is very slow
         a2 -= 1.5;
 
-
-
+        //displaying the string and the ball in the end
         pathThread1.lineTo(x1, y1);
         canvas.drawCircle(x1, y1, 20, paintCircle);
         pathThread2.lineTo(x2, y2);
         canvas.drawCircle(x2, y2, 20, paintCircle);
-
-
-
-
-
         canvas.drawPath(pathThread1, paintThread);
         canvas.drawPath(pathThread2, paintThread);
 
         invalidate();
     }
 
-    public void init() {
-
-        x1 = 1800;
-        y1 = 1800;
-
-
-    }
 }
