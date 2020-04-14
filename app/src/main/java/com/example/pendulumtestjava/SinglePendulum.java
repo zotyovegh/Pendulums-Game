102e814656127e393f59575d915710c65b063b9d
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-04-14 17:29:37 +0200

    Single Pendulum Drag and drop added
    Works well, has to solve 0 degree bug

--- app/src/main/java/com/example/pendulumtestjava/SinglePendulum.java
+++ app/src/main/java/com/example/pendulumtestjava/SinglePendulum.java
@@ -1,16 +1,14 @@
 package com.example.pendulumtestjava;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.view.ViewGroup;
+import android.view.MotionEvent;
+import android.widget.FrameLayout;
 import android.widget.TextView;
-
 import java.util.Timer;
 import java.util.TimerTask;
-
 import static android.content.ContentValues.TAG;
 
 
@@ -18,17 +16,17 @@ public class SinglePendulum extends AppCompatActivity {
     private TextView stick, ball, middle;
     private Handler handler = new Handler();
     private Timer timer = new Timer();
-
-    private  double widthMiddleBall, heightMiddleBall;
-    private  double widthMiddle, heightPoint;
+    private DrawingPath path;
+    private double widthMiddleBall, heightMiddleBall;
+    private double widthMiddle, heightPoint;
     private double r = 400;
     private double x, y;
     private double a = Math.PI / 2;
-    private DrawingPath path;
-    private float gravity = (float)0.4;
+    private float gravity = (float) 0.4;
     private double angularAcc, angularVel;
-    private float damping = (float)0.998;
-    private int trace = 100;
+    private float damping = (float) 0.999;
+    private int trace = 160;
+    private boolean onHold = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,46 +38,84 @@ public class SinglePendulum extends AppCompatActivity {
         middle = (TextView) findViewById(R.id.middlePaint);
         path = (DrawingPath) findViewById(R.id.path);
 
-        ViewGroup.LayoutParams param = stick.getLayoutParams();
-        param.height = (int)Math.round(r);
+        stick.setLayoutParams(new FrameLayout.LayoutParams(4, (int) r));
 
         widthMiddleBall = (getWindowManager().getDefaultDisplay().getWidth() / 2) - 30;
-        heightMiddleBall = (getWindowManager().getDefaultDisplay().getHeight() / 3) - 30;
+        heightMiddleBall = (getWindowManager().getDefaultDisplay().getHeight() / 2) - 30;
         widthMiddle = getWindowManager().getDefaultDisplay().getWidth() / 2;
-        heightPoint = getWindowManager().getDefaultDisplay().getHeight() / 3;
+        heightPoint = getWindowManager().getDefaultDisplay().getHeight() / 2;
 
+        update();
+    }
+
+    public void update() {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
                 handler.post(() -> {
-                    calc();
+                    if (onHold == false) {
+                        calc();
+                    }
                     draw();
                 });
             }
-        },0, 10);
+        }, 0, 10);
     }
-    public void draw(){
-        stick.setRotation((float)Math.toDegrees(-a));
-        stick.setX((float)(widthMiddle-2));
-        stick.setY((float)(heightPoint));
 
-        ball.setX((float)x);
-        ball.setY((float)y);
+    public void draw() {
+        stick.setRotation((float) Math.toDegrees(-a));
+        stick.setX((float) (widthMiddle - 2));
+        stick.setY((float) (heightPoint));
 
-        middle.setX((float)(widthMiddle -5));
-        middle.setY((float)(heightPoint - 5));
+        ball.setX((float) x);
+        ball.setY((float) y);
+
+        middle.setX((float) (widthMiddle - 5));
+        middle.setY((float) (heightPoint - 5));
         middle.setBackgroundResource(R.color.colorPrimaryDark);
     }
-    public void calc()
-    {
+
+    public void calc() {
         angularAcc = (-1 * gravity / r) * Math.sin(a);
         angularVel += angularAcc;
         angularVel *= damping;
         a += angularVel;
-
         x = widthMiddleBall + (r * Math.sin(a));
         y = heightMiddleBall + (r * Math.cos(a));
 
         path.setVariables(x, y, trace);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int newx = (int) event.getX() - 28;
+        int newy = (int) event.getY() - 90;
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+
+                onHold = true;
+                x = newx;
+                y = newy;
+                if (newy - heightPoint > 0) {
+                    a = Math.atan((newx - widthMiddleBall) / (newy - heightMiddleBall));
+
+                } else {
+                    a = Math.atan((newx - widthMiddleBall) / (newy - heightMiddleBall)) + Math.PI;
+
+                }
+                r = Math.sqrt(((newx - widthMiddleBall) * (newx - widthMiddleBall)) + ((newy - heightMiddleBall) * (newy - heightMiddleBall)));
+                stick.setLayoutParams(new FrameLayout.LayoutParams(5, (int) r));
+                path.setVariables(x, y, trace);
+                angularVel = 0;
+                angularAcc = 0;
+
+                break;
+            case MotionEvent.ACTION_UP:
+                onHold = false;
+                break;
+        }
+        return false;
     }
 }
