fc15212bfa4763eef24f60d06642cef73c4caf54
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-04-16 19:10:09 +0200

    Color picker added to settings
    Ready to install the global variables

--- app/src/main/java/com/example/pendulumtestjava/SinglePSettings.java
+++ app/src/main/java/com/example/pendulumtestjava/SinglePSettings.java
@@ -6,23 +6,44 @@ import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.core.content.ContextCompat;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class SinglePSettings extends AppCompatDialogFragment {
+    private Button colorButton;
+    private int defaultColor;
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
+
+
+
         View view = inflater.inflate(R.layout.layout_singlepsettings, null);
+    defaultColor = ContextCompat.getColor(getActivity(), R.color.colorAccent);
+        colorButton = (Button) view.findViewById(R.id.colorButton);
+        colorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("TAG", "hali");
+                openColorPicker();
+            }
+        });
+
         builder.setView(view)
 //                .setTitle("Settings")
                 .setPositiveButton("OK",
@@ -43,5 +64,20 @@ public class SinglePSettings extends AppCompatDialogFragment {
 
         return builder.create();
     }
+    public void openColorPicker()
+    {
+        AmbilWarnaDialog colorPicker = new AmbilWarnaDialog(getActivity(), defaultColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
 
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                defaultColor = color;
+                colorButton.setBackgroundColor(defaultColor);
+            }
+        });
+        colorPicker.show();
+    }
 }
