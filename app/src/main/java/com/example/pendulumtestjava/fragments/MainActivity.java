8025ac56249e6a65bb7bf05be67c4ef1c6a0231d
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-04-28 01:19:56 +0200

    Basic Navigation-drawer added
    TODO:
    - Design the drawer
    - Refactor the layout files in an ordered way

--- app/src/main/java/com/example/pendulumtestjava/fragments/MainActivity.java
+++ app/src/main/java/com/example/pendulumtestjava/fragments/MainActivity.java
@@ -1,20 +1,35 @@
 package com.example.pendulumtestjava.fragments;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.pendulumtestjava.R;
+import com.example.pendulumtestjava.firebase.FirebaseAuthActivity;
 import com.example.pendulumtestjava.fragments.savingsFragment.FragmentList;
 import com.example.pendulumtestjava.fragments.pendulumsFragments.FragmentMain;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private ViewPagerAdapter adapter;
+    private DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +45,43 @@ public class MainActivity extends AppCompatActivity{
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+
+
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.setDrawerIndicatorEnabled(true);
+        toggle.syncState();
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        if(menuItem.getItemId() == R.id.menuItem1) {
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                startActivity(new Intent(
+                                        MainActivity.this,
+                                        FirebaseAuthActivity.class));
+                                MainActivity.this.finish();
+                            } else {
+                                // Report error to user
+                            }
+                        }
+                    });
+
+        }
+        return false;
     }
 }
