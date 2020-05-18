bad2c504ea7de08e47f5c1d9c496b1219df3bd84
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-05-18 17:34:47 +0200

    Trace settings replaced with new design in double Pendulum

--- app/src/main/java/com/example/pendulumtestjava/fragments/pendulumFragments/settings/DoublePendulumSettings.java
+++ app/src/main/java/com/example/pendulumtestjava/fragments/pendulumFragments/settings/DoublePendulumSettings.java
@@ -27,12 +27,9 @@ import yuku.ambilwarna.AmbilWarnaDialog;
 public class DoublePendulumSettings extends AppCompatDialogFragment {
     private Button ballColor1, ballColor2, traceColor1, traceColor2;
     private int trace1DefaultColor, trace2DefaultColor, ball1DefaultColor, ball2DefaultColor;
-    private Switch trace1Switch, trace2Switch;
-    private CheckBox checkBoxTrace1, checkBoxTrace2;
-    private EditText trace1, trace2;
     private DoublePendulumModel data = DoublePendulumModel.getInstance();
-    private SeekBar a1b, a2b, r1b, r2b, gb, m1b, m2b;
-    private TextView a1num, a2num, r1num, r2num, gnum, m1num, m2num;
+    private SeekBar a1b, a2b, r1b, r2b, gb, m1b, m2b, trace1b, trace2b;
+    private TextView a1num, a2num, r1num, r2num, gnum, m1num, m2num, trace1num, trace2num;
 
     @SuppressLint({"SetTextI18n", "DefaultLocale", "InflateParams"})
     @NonNull
@@ -49,124 +46,191 @@ public class DoublePendulumSettings extends AppCompatDialogFragment {
         gb = view.findViewById(R.id.gb);
         m1b = view.findViewById(R.id.m1b);
         m2b = view.findViewById(R.id.m2b);
+        trace1b = view.findViewById(R.id.trace1b);
+        trace2b = view.findViewById(R.id.trace2b);
 
-        a1b.setProgress((int)Math.toDegrees(data.getA1()));
-        a2b.setProgress((int)Math.toDegrees(data.getA2()));
-        r1b.setProgress((int)data.getR1());
-        r2b.setProgress((int)data.getR2());
-        gb.setProgress((int)(data.getG()*1000));
-        m1b.setProgress((int)data.getM1());
-        m2b.setProgress((int)data.getM2());
+        a1b.setProgress((int) Math.toDegrees(data.getA1()));
+        a2b.setProgress((int) Math.toDegrees(data.getA2()));
+        r1b.setProgress((int) data.getR1());
+        r2b.setProgress((int) data.getR2());
+        gb.setProgress((int) (data.getG() * 1000));
+        m1b.setProgress((int) data.getM1());
+        m2b.setProgress((int) data.getM2());
+        trace1b.setProgress(data.getTrace1());
+        trace2b.setProgress(data.getTrace2());
 
         a1num = view.findViewById(R.id.a1num);
-        a1num.setText(String.format("%.0f",Math.toDegrees(data.getA1())));
+        a1num.setText(String.format("%.0f", Math.toDegrees(data.getA1())));
         a2num = view.findViewById(R.id.a2num);
-        a2num.setText(String.format("%.0f",Math.toDegrees(data.getA2())));
+        a2num.setText(String.format("%.0f", Math.toDegrees(data.getA2())));
         r1num = view.findViewById(R.id.r1num);
-        r1num.setText(String.format("%.0f",data.getR1()));
+        r1num.setText(String.format("%.0f", data.getR1()));
         r2num = view.findViewById(R.id.r2num);
-        r2num.setText(String.format("%.0f",data.getR2()));
+        r2num.setText(String.format("%.0f", data.getR2()));
         gnum = view.findViewById(R.id.gnum);
-        gnum.setText(String.format("%.2f", data.getG()*10));
+        gnum.setText(String.format("%.2f", data.getG() * 10));
         m1num = view.findViewById(R.id.m1num);
-        m1num.setText(String.format("%.0f",data.getM1()));
+        m1num.setText(String.format("%.0f", data.getM1()));
         m2num = view.findViewById(R.id.m2num);
-        m2num.setText(String.format("%.0f",data.getM2()));
+        m2num.setText(String.format("%.0f", data.getM2()));
+        trace1num = view.findViewById(R.id.trace1num);
+
+        if (data.getTrace1() == 30) {
+            trace1num.setText("Off");
+        } else if (data.getTrace1() == 101) {
+            trace1num.setText("Endless");
+        } else {
+            trace1num.setText(String.valueOf(data.getTrace1()));
+        }
+
+        trace2num = view.findViewById(R.id.trace2num);
+        if (data.getTrace2() == 30) {
+            trace2num.setText("Off");
+        } else if (data.getTrace2() == 101) {
+            trace2num.setText("Endless");
+        } else {
+            trace2num.setText(String.valueOf(data.getTrace2()));
+        }
 
         a1b.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 a1num.setText(String.valueOf(progress));
             }
+
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
         });
         a2b.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 a2num.setText(String.valueOf(progress));
             }
+
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
         });
         r1b.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 r1num.setText(String.valueOf(progress));
             }
+
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
         });
         r2b.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 r2num.setText(String.valueOf(progress));
             }
+
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
         });
         gb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                gnum.setText(Float.toString((float)(progress * 0.01)));
+                gnum.setText(Float.toString((float) (progress * 0.01)));
             }
+
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
         });
         m1b.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 m1num.setText(String.valueOf(progress));
             }
+
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
         });
         m2b.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 m2num.setText(String.valueOf(progress));
             }
+
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        trace1b.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (progress == 30) {
+                    trace1num.setText("Off");
+                } else if (progress == 101) {
+                    trace1num.setText("Endless");
+                } else {
+                    trace1num.setText(String.valueOf(progress));
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        trace2b.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (progress == 30) {
+                    trace2num.setText("Off");
+                } else if (progress == 101) {
+                    trace2num.setText("Endless");
+                } else {
+                    trace2num.setText(String.valueOf(progress));
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
         });
 
-        trace1 = view.findViewById(R.id.trace1);
-        trace1.setText(String.valueOf(data.getTrace1()));
-        trace2 = view.findViewById(R.id.trace2);
-        trace2.setText(String.valueOf(data.getTrace2()));
-        trace1Switch = view.findViewById(R.id.trace1Switch);
-        trace1Switch.setChecked(data.isTrace1On());
-        trace2Switch = view.findViewById(R.id.trace2Switch);
-        trace2Switch.setChecked(data.isTrace2On());
-        checkBoxTrace1 = view.findViewById(R.id.checkBoxTrace1);
-        checkBoxTrace1.setChecked(data.isEndlessTrace1());
-        checkBoxTrace2 = view.findViewById(R.id.checkBoxTrace2);
-        checkBoxTrace2.setChecked(data.isEndlessTrace2());
-
-        if (!data.isTrace1On()) {
-            trace1Switch.setChecked(false);
-        } else {
-            trace1Switch.setChecked(true);
-        }
-        if (!data.isTrace2On()) {
-            trace2Switch.setChecked(false);
-        } else {
-            trace2Switch.setChecked(true);
-        }
         trace1DefaultColor = data.getTrace1Color();
         trace2DefaultColor = data.getTrace2Color();
         ball1DefaultColor = data.getBall1Color();
@@ -196,31 +260,35 @@ public class DoublePendulumSettings extends AppCompatDialogFragment {
                             data.setR1(r1b.getProgress());
                             data.setR2(r2b.getProgress());
 
-                            data.setG((float)(gb.getProgress() * 0.01));
+                            data.setG((float) (gb.getProgress() * 0.01));
                             data.setM1(m1b.getProgress());
                             data.setM2(m2b.getProgress());
 
+                            if (trace1b.getProgress() == 30) {
+                                data.setTrace1On(false);
+                                data.setEndlessTrace1(false);
+                            } else if (trace1b.getProgress() == 101) {
+                                data.setTrace1On(true);
+                                data.setEndlessTrace1(true);
+                            } else {
+                                data.setTrace1(trace1b.getProgress());
+                                data.setTrace1On(true);
+                            }
+
+                            if (trace2b.getProgress() == 30) {
+                                data.setTrace2On(false);
+                                data.setEndlessTrace2(false);
+                            } else if (trace2b.getProgress() == 101) {
+                                data.setTrace2On(true);
+                                data.setEndlessTrace2(true);
+                            } else {
+                                data.setTrace2(trace2b.getProgress());
+                                data.setTrace2On(true);
+                            }
                             data.setBall1Color(ball1DefaultColor);
                             data.setBall2Color(ball2DefaultColor);
                             data.setTrace1Color(trace1DefaultColor);
                             data.setTrace2Color(trace2DefaultColor);
-
-                            if (trace1Switch.isChecked()) {
-                                data.setTrace1((int) Double.parseDouble(trace1.getText().toString()));
-                                data.setTrace1On(true);
-                                data.setEndlessTrace1(checkBoxTrace1.isChecked());
-                            } else {
-                                data.setTrace1On(false);
-                                data.setEndlessTrace1(checkBoxTrace1.isChecked());
-                            }
-                            if (trace2Switch.isChecked()) {
-                                data.setTrace2((int) Double.parseDouble(trace2.getText().toString()));
-                                data.setTrace2On(true);
-                                data.setEndlessTrace2(checkBoxTrace2.isChecked());
-                            } else {
-                                data.setTrace2On(false);
-                                data.setEndlessTrace2(checkBoxTrace2.isChecked());
-                            }
                         }
                 ).setNegativeButton("Cancel", (dialog, whichButton) -> dialog.dismiss()
         );
